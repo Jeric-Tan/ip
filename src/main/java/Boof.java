7ee029ade063313ea213a1958f7f321fd6b556bf
Jeric Tan <2003jeric@gmail.com>
2025-08-15 18:45:37 +0800

    Mark as Done
    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Boof.java
+++ src/main/java/Boof.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Boof {
     public static void main(String[] args) {
         String userText;
-        String[] textStorage = new String[100];
+        Task[] textStorage = new Task[100];
         int counter = 0;
         Scanner scanner = new Scanner(System.in);
 
@@ -13,25 +13,50 @@ public class Boof {
 
         while (true) {
             userText = scanner.nextLine();
+            String[] parts = userText.split(" ");
+            String command = parts[0];
 
             if (userText.equals("bye")) {
                 break;
+
             } else if (userText.equals("list")) {
-                System.out.println("   -------- Your List ---------");
+                System.out.println("    ------- Your List -------");
                 for (int i = 0; i < counter; i++) {
-                    System.out.printf("     %d. %s\n", i + 1, textStorage[i]);
+                    System.out.printf("     %d.[%s] %s\n", i + 1, textStorage[i].getStatusIcon() ,textStorage[i].description);
                 }
                 System.out.println("    -------------------------");
+
+            } else if (command.equals("mark")) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                Task t = textStorage[taskIndex];
+                
+                t.markAsDone();
+                System.out.println("    -------------------------");
+                System.out.println("      Nice! I've marked this task as done:");
+                System.out.printf("        [%s] %s\n", t.getStatusIcon(), t.description);
+                System.out.println("    -------------------------");
+
+            } else if (command.equals("unmark")) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                Task t = textStorage[taskIndex];
+
+                t.unmarkAsDone();
+                System.out.println("    -------------------------");
+                System.out.println("      OK, I've marked this task as not done yet:");
+                System.out.printf("        [%s] %s\n", t.getStatusIcon(), t.description);
+                System.out.println("    -------------------------");
+            
             } else {
                 System.out.println("    -------------------------");
-                System.out.println("added: " + userText);
+                System.out.println("        added: " + userText);
                 System.out.println("    -------------------------");
-                textStorage[counter] = userText;
+                textStorage[counter] = new Task(userText);
                 counter++;
             }
         }
-
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    -------------------------");
+        System.out.println("      Bye. Hope to see you again soon!");
+        System.out.println("    -------------------------");
         scanner.close();
 
     }
