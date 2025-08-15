e3e0cca193b78a6491b776cb87c29366e8a52450
Jeric Tan <2003jeric@gmail.com>
2025-08-15 17:48:27 +0800

    Add, List
    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Boof.java
+++ src/main/java/Boof.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Boof {
     public static void main(String[] args) {
         String userText;
+        String[] textStorage = new String[100];
+        int counter = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(
@@ -11,14 +13,26 @@ public class Boof {
 
         while (true) {
             userText = scanner.nextLine();
-            System.out.println("    ---------------------------");
-            System.out.println("    You said: " + userText);
-            System.out.println("    ---------------------------");
-            if (userText.equals("bye") || userText.equals("Bye")) {
+
+            if (userText.equals("bye")) {
                 break;
+            } else if (userText.equals("list")) {
+                System.out.println("   -------- Your List ---------");
+                for (int i = 0; i < counter; i++) {
+                    System.out.printf("     %d. %s\n", i + 1, textStorage[i]);
+                }
+                System.out.println("    -------------------------");
+            } else {
+                System.out.println("    -------------------------");
+                System.out.println("added: " + userText);
+                System.out.println("    -------------------------");
+                textStorage[counter] = userText;
+                counter++;
             }
         }
+
         System.out.println("Bye. Hope to see you again soon!");
         scanner.close();
 
-    }}
+    }
+}
