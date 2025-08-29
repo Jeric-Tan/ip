6f8334ba7b554160c4201bee448c4b21aa90f208
Jeric Tan <2003jeric@gmail.com>
2025-08-29 15:24:38 +0800

    Update Boof.java

--- src/main/java/boof/Boof.java
+++ src/main/java/boof/Boof.java
@@ -30,79 +30,58 @@ public class Boof {
         boolean isExit = false;
         while (!isExit) {
             String userText = ui.readCommand();
-            String[] parts = userText.split(" ");
-            String command = Parser.getCommand(userText);
-
-            switch (command) {
-                case "bye":
-                    isExit = true;
-                    ui.showExit();
-                    break;
-                case "list":
-                    ui.showMessage("    ------- Your List -------");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        ui.showMessage("     " + (i + 1) + "." + tasks.get(i).toString());
-                    }
-                    ui.displayLine();
-                    break;
-                case "mark": {
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    Task t = tasks.get(taskIndex);
-                    t.markAsDone();
-                    storage.save(tasks.getAll());
-                    ui.displayLine();
-                    ui.showMessage("      Nice! I've marked this task as done:");
-                    ui.showMessage("        " + t.toString());
-                    ui.displayLine();
-                    break;
-                }
-                case "unmark": {
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    Task t = tasks.get(taskIndex);
-                    t.unmarkAsDone();
-                    storage.save(tasks.getAll());
-                    ui.displayLine();
-                    ui.showMessage("      OK, I've marked this task as not done yet:");
-                    ui.showMessage("        " + t.toString());
-                    ui.displayLine();
-                    break;
-                }
-                case "todo": {
-                    if (parts.length < 2) {
+            Parser.CommandType commandType = Parser.getCommandType(userText);
+            try {
+                switch (commandType) {
+                    case BYE:
+                        isExit = true;
+                        ui.showExit();
+                        break;
+                    case LIST:
+                        ui.showMessage("    ------- Your List -------");
+                        for (int i = 0; i < tasks.size(); i++) {
+                            ui.showMessage("     " + (i + 1) + "." + tasks.get(i).toString());
+                        }
                         ui.displayLine();
-                        ui.showMessage("      OOPS!!! The description of a todo cannot be empty.");
+                        break;
+                    case MARK: {
+                        int taskIndex = Parser.parseIndex(userText) - 1;
+                        Task t = tasks.get(taskIndex);
+                        t.markAsDone();
+                        storage.save(tasks.getAll());
+                        ui.displayLine();
+                        ui.showMessage("      Nice! I've marked this task as done:");
+                        ui.showMessage("        " + t.toString());
                         ui.displayLine();
                         break;
                     }
-                    String description = userText.substring(5);
-                    Task newTask = new Todo(description);
-                    tasks.add(newTask);
-                    storage.save(tasks.getAll());
-                    ui.displayLine();
-                    ui.showMessage("      Got it. I've added this task:");
-                    ui.showMessage("        " + newTask.toString());
-                    ui.showMessage("      Now you have " + tasks.size() + " tasks in the list.");
-                    ui.displayLine();
-                    break;
-                }
-                case "deadline": {
-                    if (parts.length < 2) {
+                    case UNMARK: {
+                        int taskIndex = Parser.parseIndex(userText) - 1;
+                        Task t = tasks.get(taskIndex);
+                        t.unmarkAsDone();
+                        storage.save(tasks.getAll());
                         ui.displayLine();
-                        ui.showMessage("      OOPS!!! The description of a deadline cannot be empty.");
+                        ui.showMessage("      OK, I've marked this task as not done yet:");
+                        ui.showMessage("        " + t.toString());
                         ui.displayLine();
                         break;
                     }
-                    String[] deadlineParts = userText.split(" /by ", 2);
-                    if (deadlineParts.length < 2) {
+                    case TODO: {
+                        String description = Parser.parseTodoDescription(userText);
+                        Task newTask = new Todo(description);
+                        tasks.add(newTask);
+                        storage.save(tasks.getAll());
                         ui.displayLine();
-                        ui.showMessage(
-                                "      OOPS!!! Please specify a /by date in the correct format (e.g., 2019-10-15 1800).");
+                        ui.showMessage("      Got it. I've added this task:");
+                        ui.showMessage("        " + newTask.toString());
+                        ui.showMessage("      Now you have " + tasks.size() + " tasks in the list.");
                         ui.displayLine();
                         break;
                     }
-                    String description = deadlineParts[0].substring(9);
-                    String byDate = deadlineParts[1];
-                    try {
+                    case DEADLINE: {
+                        String[] deadlineParts = Parser.parseDeadlineCommand(userText);
+                        String description = deadlineParts[0];
+                        String byDate = deadlineParts[1];
                         Task newTask = new Deadline(description, byDate);
                         tasks.add(newTask);
                         storage.save(tasks.getAll());
@@ -111,63 +90,50 @@ public class Boof {
                         ui.showMessage("        " + newTask.toString());
                         ui.showMessage("      Now you have " + tasks.size() + " tasks in the list.");
                         ui.displayLine();
-                    } catch (Exception e) {
+                        break;
+                    }
+                    case EVENT: {
+                        String[] eventParts = Parser.parseEventCommand(userText);
+                        String description = eventParts[0];
+                        String from = eventParts[1];
+                        String to = eventParts[2];
+                        Task newTask = new Event(description, from, to);
+                        tasks.add(newTask);
+                        storage.save(tasks.getAll());
+                        ui.displayLine();
+                        ui.showMessage("      Got it. I've added this task:");
+                        ui.showMessage("        " + newTask.toString());
+                        ui.showMessage("      Now you have " + tasks.size() + " tasks in the list.");
+                        ui.displayLine();
+                        break;
+                    }
+                    case DELETE: {
+                        int taskIndex = Parser.parseIndex(userText) - 1;
+                        if (taskIndex < 0 || taskIndex >= tasks.size()) {
+                            ui.displayLine();
+                            ui.showMessage("      OOPS!!! Task number does not exist.");
+                            ui.displayLine();
+                            break;
+                        }
+                        Task deletedTask = tasks.remove(taskIndex);
+                        storage.save(tasks.getAll());
+                        ui.displayLine();
+                        ui.showMessage("      Noted. I've removed this task:");
+                        ui.showMessage("        " + deletedTask.toString());
+                        ui.showMessage("      Now you have " + tasks.size() + " tasks in the list.");
+                        ui.displayLine();
+                        break;
+                    }
+                    default:
                         ui.displayLine();
                         ui.showMessage(
-                                "      OOPS!!! Invalid date format. Please use yyyy-MM-dd HHmm or d/M/yyyy HHmm.");
+                                "        Idk what that means! Use either todo deadline or event to create a task!");
                         ui.displayLine();
-                    }
-                    break;
                 }
-                case "event": {
-                    if (parts.length < 2) {
-                        ui.displayLine();
-                        ui.showMessage("      OOPS!!! The description of an event cannot be empty.");
-                        ui.displayLine();
-                        break;
-                    }
-                    String[] eventSplit = userText.split(" /from ");
-                    String description = eventSplit[0].substring(6);
-                    String[] fromTo = eventSplit[1].split(" /to ");
-                    String from = fromTo[0];
-                    String to = fromTo.length > 1 ? fromTo[1] : "";
-                    Task newTask = new Event(description, from, to);
-                    tasks.add(newTask);
-                    storage.save(tasks.getAll());
-                    ui.displayLine();
-                    ui.showMessage("      Got it. I've added this task:");
-                    ui.showMessage("        " + newTask.toString());
-                    ui.showMessage("      Now you have " + tasks.size() + " tasks in the list.");
-                    ui.displayLine();
-                    break;
-                }
-                case "delete": {
-                    if (parts.length < 2) {
-                        ui.displayLine();
-                        ui.showMessage("      OOPS!!! Specify the task number to delete.");
-                        ui.displayLine();
-                        break;
-                    }
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex < 0 || taskIndex >= tasks.size()) {
-                        ui.displayLine();
-                        ui.showMessage("      OOPS!!! Task number does not exist.");
-                        ui.displayLine();
-                        break;
-                    }
-                    Task deletedTask = tasks.remove(taskIndex);
-                    storage.save(tasks.getAll());
-                    ui.displayLine();
-                    ui.showMessage("      Noted. I've removed this task:");
-                    ui.showMessage("        " + deletedTask.toString());
-                    ui.showMessage("      Now you have " + tasks.size() + " tasks in the list.");
-                    ui.displayLine();
-                    break;
-                }
-                default:
-                    ui.displayLine();
-                    ui.showMessage("        Idk what that means! Use either todo deadline or event to create a task!");
-                    ui.displayLine();
+            } catch (Exception e) {
+                ui.displayLine();
+                ui.showMessage("      OOPS!!! " + e.getMessage());
+                ui.displayLine();
             }
         }
     }
